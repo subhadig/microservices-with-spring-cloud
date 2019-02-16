35fef3d6259b80b8d5953ac7cfdf536f0c315a49
Subhadip Ghosh <subhadip.sky@gmail.com>
2019-02-16 10:52:34 +0530

    Initial commit.

---
+++ currencyconversion/src/main/java/com/subhadip/microservices/currencyconversion/CurrencyconversionApplication.java
@@ -0,0 +1,18 @@
+package com.subhadip.microservices.currencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients( "com.subhadip.microservices.currencyconversion" )
+@EnableDiscoveryClient
+public class CurrencyconversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionApplication.class, args);
+	}
+
+}
+
---
+++ currencyconversion/src/test/java/com/subhadip/microservices/currencyconversion/CurrencyconversionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.subhadip.microservices.currencyconversion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyconversionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
